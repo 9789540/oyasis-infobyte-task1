17092fa90772f761937eb203a200fa610b7bf0c4
9789540 <162969143+9789540@users.noreply.github.com>
2024-05-05 13:44:58 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,47 +1,25 @@
-const converter = document.querySelector('#converter');
-const result = document.querySelector('.result');
-const convertButton = document.querySelector('.convertButton');
-const resetButton = document.querySelector('.resetButton');
-const changeButton = document.querySelector('.changeButton');
-const C = document.querySelector('.C')
-const F = document.querySelector('.F')
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
 
-const swap = () => {
-    if (C.innerHTML === '°C') {
-        C.innerHTML = '°F';
-        F.innerHTML = '°C';
-        converter.placeholder = "Temperature in °F";
-    }else {
-        F.innerHTML = '°F';
-        C.innerHTML = '°C';
-        converter.placeholder = "Temperature in °C";
-    };
-};
 
-const reset = () => {
-    converter.value = '';
-    result.innerHTML = '';
-}
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
 
-const convert = () => {
-    if (/^(-?)(\d+)(\.{1}\d+)?$/.test(converter.value)) {
-        result.style.color = "black"
-        if (C.innerHTML === '°C') {
-            let value = (converter.value * 1.8) + 32;
-            result.innerHTML = `${converter.value}°C is equal to ${value.toFixed(2)}°F`;
-        } else {
-            let value = (converter.value - 32) / 1.8;
-            result.innerHTML = `${converter.value}°F is equal to ${value.toFixed(2)}°C`;
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a [href*=' + id + ' ]').classList.add('active');
+            })
         }
-    } else if (converter.value == '') {
-        result.style.color = "#993300"
-        result.innerHTML = 'Enter some number';
-    } else {
-        result.style.color = "#993300"
-        result.innerHTML = 'Enter only numbers!!!';
-    }
+    })
 }
-
-changeButton.addEventListener('click', swap);
-resetButton.addEventListener('click', reset);
-convertButton.addEventListener('click', convert);
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
